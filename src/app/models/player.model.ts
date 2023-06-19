f99e9afc2d10efbc4c716f2b3343fb3e10c2af4f
Gabriel Neves <gabriel.santos@dellead.com>
2023-06-19 10:56:58 -0300

    Removing model not used

--- src/app/models/player.model.ts
+++ src/app/models/player.model.ts
@@ -1,16 +1,6 @@
-import { AbstractMesh, Camera, Vector3 } from "babylonjs";
+import { AbstractMesh, Camera } from "babylonjs";
 
 export interface Player {
     body: AbstractMesh;
     camera?: Camera;
-}
-
-export abstract class PlayerPosition {
-    private x: number;
-    private y: number;
-    private z: number;
-
-    public toVector3() {
-        return new Vector3(this.x, this.y, this.z);
-    }
 }
